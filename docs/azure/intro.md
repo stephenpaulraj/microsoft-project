552f8ae2dcd3c8ef5d3c181ca430896792bfba7a
Stephen Paulraj <stephenpaulraj@live.com>
2023-01-09 02:19:36 +0530

    first commit

---
+++ docs/azure/intro.md
@@ -0,0 +1,46 @@
+---
+sidebar_position: 1
+---
+
+# Question Bank
+
+**Use search bar for quick access to Q & A.**
+
+## Question 1
+
+**You are authoring a set of nested azure resource manager templates to deploy multiple azure resources. The templates must be tested before deployment and must follow recommended practices. You need to validate and test the templates before deployment, which tools should u use to answer drag the appropriate tools to the correct requirements ,each tool maybe used once, more than once or not at all. You may need to drag the split bar between panes or scroll to view content.**
+
+1. Parameter file
+2. Template function
+3. Azure resource manager test tool kit
+4. User defined function
+5. What-if operation
+6. Azure deployment manager
+
+:::tip Answer
+
+1. azure resource manager test tool kit
+2. What-if operation
+
+:::
+
+
+## Question 2
+
+**You are developing an Azure Durable Function to manage an online ordering process.The process must call an external API to gather product discount information. You need to implement the Azure Durable Function. Which Azure Durable Function types should you use? Each correct answer presents part of the solution. NOTE: Each correct selection is worth one point.**
+
+1. Orchestrator
+2. Entity
+3. Client
+4. Activity
+
+
+:::tip Answer
+
+A & B (1 & 2)
+
+:::
+
+
+
+
---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Microsoft Exam Project",
+  tagline: "Azure, DevOps and Data Engineer",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "RV Matrix", // Usually your GitHub org/user name.
+  projectName: "microsoft-exam-project", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      algolia: {
+        appId: "BQH2SXBYA8",
+        apiKey: "377f380c6b03bc6d98bd54f2ecdc1d93",
+        indexName: "microsoft_bit",
+        contextualSearch: false,
+      },
+      navbar: {
+        logo: {
+          alt: "RV Matrix",
+          src: "img/rvm.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "azure/intro",
+            position: "left",
+            label: "Azure",
+          },
+          {
+            type: "doc",
+            docId: "dev-ops/intro",
+            position: "left",
+            label: "DevOps Engineer",
+          },
+          {
+            type: "doc",
+            docId: "data-engineer/intro",
+            position: "left",
+            label: "Data Engineer",
+          },
+        ],
+      },
+      footer: {
+        copyright: `Copyright © ${new Date().getFullYear()}. RV Matrix.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
